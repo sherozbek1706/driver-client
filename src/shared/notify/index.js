0a46769d5e353b75737882644297d1b415a433f2
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-11 18:44:43 +0500

    success notify

--- src/shared/notify/index.js
+++ src/shared/notify/index.js
@@ -25,3 +25,15 @@ export const warning_notify = (msg) => {
     theme: "dark",
   });
 };
+export const success_notify = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
