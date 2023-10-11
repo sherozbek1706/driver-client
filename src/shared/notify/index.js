ee6354c1072a8065110024db3247dc70fb295fd5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-11 18:44:13 +0500

    error notify

---
+++ src/shared/notify/index.js
@@ -0,0 +1,15 @@
+import { toast } from "react-toastify";
+
+export const error_notify = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
