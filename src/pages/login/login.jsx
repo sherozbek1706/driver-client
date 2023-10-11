e9e9f5b216c00371d49af559b329ca7a2628a4c0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-11 12:35:48 +0500

    Asosiy template yasb olindi. Login qilish uchun

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,6 +3,18 @@ export const Login = () => {
   return (
     <div className="LoginPage">
       <div class="form-wrapper">
+        <h2>Login</h2>
+        <form action="#">
+          <div class="form-control">
+            <input type="text" required />
+            <label>Login</label>
+          </div>
+          <div class="form-control">
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
     </div>
   );
