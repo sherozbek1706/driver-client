32a085141cef995e530ddd8d33727cc1247ebe68
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-11 18:44:28 +0500

    warning notify

--- src/shared/notify/index.js
+++ src/shared/notify/index.js
@@ -13,3 +13,15 @@ export const error_notify = (msg) => {
   });
 };
 
+export const warning_notify = (msg) => {
+  toast.warn(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
