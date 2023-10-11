1bcbe5d308c0abbbba560807f37bbea9e76bf05d
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-11 18:47:22 +0500

    routerlarni himoya qilish uchun middlerare

---
+++ src/auth/protect-driver/index.js
@@ -0,0 +1,6 @@
+import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const ProtectRouteDriver = ({ children }) => {
+};
