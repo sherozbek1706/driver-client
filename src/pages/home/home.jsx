a4b2c8e2569c0b133be5bfa80fbc7f60e84f3b8b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-14 21:00:17 +0500

    home page uchun jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { Header } from "../../components";
 import { Sitebar } from "../../layouts";
 import "./home.css";
+import Cookies from "js-cookie";
 export const Home = () => {
+  // useEffect(() => {
+  //   Object.keys(Cookies.get()).forEach(function (cookieName) {
+  //     Cookies.remove(cookieName);
+  //   });
+  // }, []);
   return (
     <div className="Home">
+      <Header title={"Bosh Sahifa"} />
       <Sitebar />
     </div>
   );
