13ad607a0db0fc877cc72160dd24478290302973
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-15 12:43:25 +0500

    datani map qilib yubordim.

--- src/pages/maps/maps.jsx
+++ src/pages/maps/maps.jsx
@@ -6,9 +6,25 @@ import { axiosInstance } from "../../shared/services";
 import { errorHandler } from "../../shared/handler/errors";
 export const Maps = () => {
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
   useEffect(() => {
     setLoading(true);
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axiosInstance.get("/address");
+      const base = response.data.data;
+      setData(base);
+      if (response.status == 200) {
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      errorHandler(error, changeActive);
+    }
+  };
 
   if (loading) {
     return <Loader />;
@@ -18,6 +34,17 @@ export const Maps = () => {
     <div className="Maps">
       <Header title={"Manzillar"} />
       <div className="Maps__container">
+          <Fragment>
+            {data.map((item) => (
+              <div className="Maps__box" key={item.id}>
+                <h3 className="Maps__address">{item.address}</h3>
+                <div className="Maps__details">
+                  <i className="fa-solid fa-ellipsis icon"></i>
+                </div>
+              </div>
+            ))}
+            <h1 className="Maps__count">- {data.length} ta manzil topildi -</h1>
+          </Fragment>
       </div>
       <Sitebar />
     </div>
