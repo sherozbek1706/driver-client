f5ee78d1e8d3b14368d5f7c18b474b113ba1a745
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-19 16:57:31 +0500

    socket io ulandi.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Cookies from "js-cookie";
 import "./App.css";
 import { Router } from "./shared";
+import React, { useEffect } from "react";
+import { api } from "./utils";
+import io from "socket.io-client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+export const socket = io.connect(api);
+
+// export const Context = React.createContext();
 function App() {
+  // useEffect(() => {
+  //   Cookies.set("language", "kiril");
+  // }, []);
+  // const context_data = {
+  //   lang: Cookies.get("language"),
+  // };
+
   return (
+    // <Context.Provider value={context_data}>
     <div className="App">
       <Router />
       <ToastContainer
@@ -20,6 +35,7 @@ function App() {
         theme="dark"
       />
     </div>
+    // </Context.Provider>
   );
 }
 
