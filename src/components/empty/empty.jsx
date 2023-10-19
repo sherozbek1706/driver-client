936ba04c121faaece004b1179c740ac29005e4ec
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-19 16:55:03 +0500

    hech narsa topilmasa bajariladigan sahifa qilindi.

---
+++ src/components/empty/empty.jsx
@@ -0,0 +1,11 @@
+import "./empty.css";
+export const Empty = () => {
+  return (
+    <div className="Empty">
+      <i class="fa-solid fa-box-open icon"></i>
+      {/* <i class="fa-solid fa-inbox icon"></i> */}
+      <h1>Buyurma topilmadi.</h1>
+      <p>Topilishi bilan birinchi bo'lib sizga xabar berialdi, Sabr qiling!</p>
+    </div>
+  );
+};
