7d6eefa4a35cffb6b8cd6e74dfeb4acf1b627b32
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-19 16:55:51 +0500

    headerda kiril alifbosidan foydalanildi.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,16 @@
 import "./header.css";
+// import { useContext } from "react";
+// import { lotinKirilOtkazish } from "../../utils/functions/lotin-kiril";
+// import { Context } from "../../App";
 export const Header = ({ title }) => {
+  // const data = useContext(Context);
+
   return (
     <div className="Header">
-      <h1 className="Header__title">{title}</h1>
+      <h1 className="Header__title">
+        {/* {data.lang == "kiril" ? lotinKirilOtkazish(title) : title} */}
+        {title}
+      </h1>
     </div>
   );
 };
