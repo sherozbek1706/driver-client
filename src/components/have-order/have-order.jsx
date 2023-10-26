f364ac11c3667a50787e798b755e980927df14c7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-26 20:16:16 +0500

    tilini o'zgartirdim , lotincha kirilcha

--- src/components/have-order/have-order.jsx
+++ src/components/have-order/have-order.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import "./have-order.css";
+import { lotinKirilOtkazish } from "../../utils/functions/lotin-kiril";
 export const HaveOrder = ({ id }) => {
   return (
     <div className="HaveOrder">
       <i className="fa-solid fa-truck-fast icon"></i>
-      <h1>Tugatilmagan buyurtma mavjud!</h1>
+      <h1>{lotinKirilOtkazish("Tugatilmagan buyurtma mavjud!")}</h1>
       <p>
-        Ishlashni xoxlasangiz, "Buyutmaga o'tish" tugmasini bosib, buyurtmani
-        bajarib bo'lib "Buyurtmani tugatish" - tugmasini bosing.
+        {lotinKirilOtkazish(`Ishlashni xoxlasangiz, "Buyutmaga o'tish" tugmasini bosib, buyurtmani
+        bajarib bo'lib "Buyurtmani tugatish" - tugmasini bosing.`)}
       </p>
-      <Link to={"/orders/" + id}>Buyutmaga o'tish</Link>
+      <Link to={"/orders/" + id}>{lotinKirilOtkazish("Buyutmaga o'tish")}</Link>
     </div>
   );
 };
