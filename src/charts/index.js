739af7f32e3ece9d43d933bca93036fafc1ee3fe
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-26 20:14:18 +0500

    haydovchilarni qo'shilgani elon qiluvchi grafik

--- src/charts/index.js
+++ src/charts/index.js
@@ -1,2 +1,5 @@
 export * from "./drivers-chart/drivers-chart";
 export * from "./daily-order-chart/daily-order-chart";
+export * from "./new-drivers-chart/new-drivers-chart";
+export * from "./daily-paid/daily-paid";
+export * from "./daily-work/daily-work";
