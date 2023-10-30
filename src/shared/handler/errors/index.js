05c5068e1dc5718610768e928c0048afc9169861
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-30 11:00:33 +0500

    errorlar uchun js

--- src/shared/handler/errors/index.js
+++ src/shared/handler/errors/index.js
@@ -1,26 +1,60 @@
 import Cookies from "js-cookie";
-import { error_notify } from "../../notify";
-export const errorHandler = (error, changeActive) => {
-  let response = error.response;
-  if (response.status == 401) {
+import { error_notify, info_notify, warning_notify } from "../../notify";
+export const errorHandler = (error, callFunction) => {
+  let response = error?.response;
+
+  if (!error.response) {
+    error_notify(error.message);
+  }
+
+  if (error.response.data.error) {
+    error_notify(error.response.data.error);
+  }
+
+  if (response?.status === 401) {
     error_notify("Qaytadan tizimga kiring!");
-    setTimeout(() => {
+    Object.keys(Cookies.get()).forEach((cookieName) => {
+      Cookies.remove(cookieName);
+    });
+    // setTimeout(() => {
+    window.location.assign("/login");
+    // }, 1000);
+  }
+
+  if (error.message === "Network Error") {
+    error_notify("Malumoatlar kelmayabdi!");
+  }
+
+  if (response?.status === 400) {
+    error_notify("So'rov xato yuborildi!");
+
+    if (response?.data.error === "Bunday buyurtma mavjud emas!") {
+      return callFunction();
+    }
+  }
+
+  if (response?.status === 403) {
+    if (
+      response?.data.error === "Ushbu haydovchi hozirda active holatida emas!"
+    ) {
+      return callFunction();
+    }
+    if (response?.data.error === "Buyurtmani o'chirish mumkin emas!") {
+      info_notify("bajarilgan buyurtmani o'chirish mumkin emas!");
+    }
+
+    if (response?.data.error === "Ushbu haydovchi hozirda blok holatida!") {
+      warning_notify("Admin sizni bloklagan!");
+      window.location.assign("/");
       Object.keys(Cookies.get()).forEach(function (cookieName) {
         Cookies.remove(cookieName);
       });
-    }, 2100);
-  }
-
-  if (response.status == 400) {
-    error_notify("So'rov xato yuborildi!");
-  }
-
-  if (response.status == 403) {
-    if (
-      response.data.error == "Ushbu haydovchi hozirda active holatida emas!"
-    ) {
-      error_notify("Siz hozir ishda emassiz!");
-      return changeActive();
     }
   }
+
+  // if (response.status === 402) {
+  //   if (response.data.error === "Mablag' yetarli emas!") {
+  //     return callFunction();
+  //   }
+  // }
 };
