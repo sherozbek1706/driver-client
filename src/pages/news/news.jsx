27d6fc0c0f0ba4f721d2adf42e44c64a6cbce133
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-30 10:58:13 +0500

    yangiliklar sahifasi uchun jsx

--- src/pages/news/news.jsx
+++ src/pages/news/news.jsx
@@ -1,10 +1,14 @@
-import { Header } from "../../components";
+import { useState } from "react";
+import { Build, Header } from "../../components";
 import { Sitebar } from "../../layouts";
 import "./news.css";
 export const News = () => {
+  const [isBuild, setBuild] = useState(true);
+
   return (
     <div className="News">
       <Header title={"Yangiliklar"} />
+      {isBuild ? <Build /> : ""}
       <Sitebar />
     </div>
   );
