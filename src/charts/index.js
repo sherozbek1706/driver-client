193c8837cb354cfd063bbf4c54ef4197cda5c2d5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-30 10:24:40 +0500

    kunlik to'lovlarni amalga oshirish bo'yicha charts

--- src/charts/index.js
+++ src/charts/index.js
@@ -3,3 +3,4 @@ export * from "./daily-order-chart/daily-order-chart";
 export * from "./new-drivers-chart/new-drivers-chart";
 export * from "./daily-paid/daily-paid";
 export * from "./daily-work/daily-work";
+export * from "./daily-payment/daily-payment";
