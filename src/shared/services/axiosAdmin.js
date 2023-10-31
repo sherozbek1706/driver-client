6d33eb3545a9b1e34b58dba793c4e28e45a4e28f
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-31 17:08:24 +0500

    cor bilan muammoni o'zgaritirhs kiritildi

--- src/shared/services/axiosAdmin.js
+++ src/shared/services/axiosAdmin.js
@@ -5,6 +5,7 @@ const ACCESS_TOKEN = Cookies.get("admin_token") || "";
 export const axiosAdmin = axios.create({
   baseURL: api,
   headers: {
+    "Access-Control-Allow-Origin": "*",
     "Content-type": "application/json",
     Authorization: ACCESS_TOKEN,
   },
