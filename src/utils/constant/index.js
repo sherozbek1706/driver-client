a5c8e1309efd0f773ee04c6ff3351d51fd6c0cca
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2023-12-04 16:01:36 +0500

    Umumiy build qilindi

--- src/utils/constant/index.js
+++ src/utils/constant/index.js
@@ -1,3 +1,3 @@
 export const api = "https://api.sherozbek.uz";
-// export const api = `http://localhost:5000/`;
-// export const api = `http://192.168.205.157:5000/`;
+// export const api = `http://localhost:5000`;
+// export const api = `http://192.168.1.40:5000`;
