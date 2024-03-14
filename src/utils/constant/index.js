0bd40b7f728cb6d3424d187151fe9ff26d7e8f38
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2024-03-14 14:48:47 +0500

    Backned api env ichiga olindi

--- src/utils/constant/index.js
+++ src/utils/constant/index.js
@@ -1,3 +1,1 @@
-export const api = "https://api.sherozbek.uz";
-// export const api = `http://localhost:5000`;
-// export const api = `http://192.168.1.40:5000`;
+export const api = process.env.REACT_APP_BACKEND_URL;
